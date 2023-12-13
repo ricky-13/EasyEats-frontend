7bf5a1f9e264d4acdee2661b4f63f9123506347c
Ricky <ahujashaurya13@gmail.com>
2023-12-13 02:53:34 -0500

    displaying data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  let options = props.options;
+  let priceOption = Object.keys(options);
+
   return (
     <div>
       <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-        <img
-          className="card-img-top"
-          src="https://images.pexels.com/photos/2456435/pexels-photo-2456435.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="alt text"
-        />
+        <img className="card-img-top" src={props.imgSrc} alt="alt text" />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">This is some important text.</p>
+          <h5 className="card-title">{props.foodName}</h5>
+          {/* <p className="card-text">This is some important text.</p> */}
           <div className="container w-100">
             <select className="m-2 h-100 bg-success rounded">
               {Array.from(Array(8), (e, i) => {
@@ -24,8 +23,13 @@ export default function Card() {
             </select>
 
             <select className="m-2 h-100 bg-success rounded">
-              <option value="Half">Half</option>
-              <option value="Full">Full</option>
+              {priceOption.map((data) => {
+                return (
+                  <option key={data} value={data}>
+                    {data}
+                  </option>
+                );
+              })}
             </select>
 
             <div className="d-inline h-100 fs-4">Total Price</div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,7 @@ import Card from "../components/Card";
 import Carousel from "../components/Carousel";
 
 export default function Home() {
+  const [search, setSearch] = useState('');
   // .map function only applicable with arrays and not objects, that is why no curly braces in useState!
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
@@ -37,25 +38,102 @@ export default function Home() {
       </div>
 
       <div>
-        <Carousel />
+      <div
+        id="carouselExampleControls"
+        className="carousel slide"
+        data-bs-ride="carousel"
+        style={{ objectFit: "contain !important" }}
+      >
+        <div className="carousel-inner" id="carousel">
+          <div className="carousel-caption" style={{ zIndex: "10" }}>
+            <div className="d-flex justify-content-center">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                value={search}
+                onChange={(e) => {setSearch(e.target.value)}}
+              />
+              {/* <button
+                className="btn btn-outline-success text-white "
+                type="submit"
+                // style={{ backgroundColor: "blue" }}
+              >
+                Search
+              </button> */}
+            </div>
+          </div>
+          <div className="carousel-item active">
+            <img
+              className="d-block w-100"
+              src="https://source.unsplash.com/random/900x700/?burger"
+              alt="First slide"
+              style={{ filter: "brightness(50%)" }}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="https://source.unsplash.com/random/900x700/?pasta"
+              alt="Second slide"
+              style={{ filter: "brightness(50%)" }}
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="https://source.unsplash.com/random/900x700/?pizza"
+              alt="Third slide"
+              style={{ filter: "brightness(50%)" }}
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          data-bs-target="#carouselExampleControls"
+          type="button"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          data-bs-target="#carouselExampleControls"
+          type="button"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
       </div>
 
       <div className="container">
-        {
-          
+        {    
           foodCat !== []
             ? foodCat.map((data) => {
             return (
-              <div>
+              <div className="row mb-3">
                 <div key = {data._id} className="fs-3 m-3"> 
                   {data.CategoryName} 
                 </div>
                 <hr />
-                {foodItem !== []? foodItem.filter((item) => item.CategoryName == data.CategoryName)
+                {foodItem !== []? foodItem.filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toLowerCase().includes(search.toLocaleLowerCase())))
                 .map(filterItems => {
                   return(
-                    <div key = {filterItems._id}>
-                      <Card></Card>
+                    <div key = {filterItems._id} className="col-12 col-md-6 col-lg-3">
+                      <Card foodName = {filterItems.name}
+                      options={filterItems.options[0]}
+                      imgSrc={filterItems.img}>
+                      </Card>
                     </div>
                   )
                 }): <div> No such data found </div>}
@@ -64,7 +142,6 @@ export default function Home() {
             })
           : ""
         }
-        <Card />
       </div>
 
       <div>
